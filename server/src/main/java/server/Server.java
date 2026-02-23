9a32070b8fa742726550c95323cc172790cb800a
Trevor Bagley <t@tdo90.com>
2026-02-23 14:31:13 -0700

    initial commit + some handler

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,27 @@
+package server;
+
+import io.javalin.*;
+
+public class Server {
+
+    private final Javalin javalin;
+
+    public Server() {
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
+        // Register your endpoints and exception handlers here.
+        javalin.post("/user", ctx -> {
+            ctx.result("Register endpoint hit");
+        });
+
+    }
+
+    public int run(int desiredPort) {
+        javalin.start(desiredPort);
+        return javalin.port();
+    }
+
+    public void stop() {
+        javalin.stop();
+    }
+}
