4061fb99378ab9c521288781525edfc54719730b
Trevor Bagley <t@tdo90.com>
2026-03-20 12:29:46 -0600

    adding helper methods to help make the generic method

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,12 +1,10 @@
 package client;
 
 import com.google.gson.Gson;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
@@ -14,6 +12,8 @@ import model.*;
 
 public class ServerFacade {
     private final String serverUrl;
+    private Gson gson = new Gson();
+
 
     public ServerFacade(String serverUrl) {
         this.serverUrl = serverUrl;
@@ -21,21 +21,22 @@ public class ServerFacade {
 
     public RegisterResponse register(RegisterRequest registerRequest) throws ResponseException {
         try {
-            URL url = (URI.create(serverUrl + "/user")).toURL();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            // configure URL
+            URL url = configureUrl("/user");
 
-            http.setRequestMethod("POST");
-            http.setDoOutput(true);
-            http.addRequestProperty("Content-Type", "application/json");
+            // handle httpConnection
+            HttpURLConnection http = configureHttp(url, "POST");
 
-            Gson gson = new Gson();
-            String jsonRequest = gson.toJson(registerRequest);
+            // serialize json
+            String jsonRequest = serializeJson(registerRequest);
 
+            // write body
             try (OutputStream reqBody = http.getOutputStream();
                  Writer writer = new OutputStreamWriter(reqBody)) {
                 writer.write(jsonRequest);
             }
 
+            // handle response code
             int status = http.getResponseCode();
 
             if (status == 200) {
@@ -46,13 +47,42 @@ public class ServerFacade {
             } else {
                 throw new ResponseException("Registration failed: " + status);
             }
-
         } catch (java.io.IOException e) {
             throw new ResponseException("Unable to communicate with server");
         } catch (IllegalArgumentException e) {
             throw new ResponseException("Invalid server URL");
         } catch (com.google.gson.JsonSyntaxException e) {
             throw new ResponseException("Invalid response from server");
+        }
+    }
+
+    private <T> String serializeJson (T requestObj) throws ResponseException {
+        try {
+            return gson.toJson(requestObj);
+        } catch (Exception e) {
+            throw new ResponseException("Error: Failed Serializing JSON");
+        }
+    }
+
+    private HttpURLConnection configureHttp (URL url, String method) throws ResponseException {
+        try {
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+            http.addRequestProperty("Content-Type", "application/json");
+            return http;
+        } catch (Exception e) {
+            throw new ResponseException("Error: Http connection failed");
+        }
+    }
+
+    private URL configureUrl (String urlSring) throws ResponseException  {
+        try {
+            return (URI.create(serverUrl + urlSring)).toURL();
+        } catch (IllegalArgumentException e) {
+            throw new ResponseException("Error: Illegal argument in server URL");
+        } catch (MalformedURLException e) {
+            throw new ResponseException("Error: MalformedURL in server URL");
         }
     }
 
