44b4c226148e60f46bbafda4723bfe2eb22b356d
Trevor Bagley <t@tdo90.com>
2026-03-22 18:46:49 -0600

    preparing to send request to server

--- client/src/main/java/ui/Menu.java
+++ client/src/main/java/ui/Menu.java
@@ -5,9 +5,13 @@ import java.util.regex.Pattern;
 
 public class Menu {
     private STATE state = STATE.loggedOut;
+    private final Scanner scanner;
+
+    public Menu () {
+        this.scanner = new Scanner(System.in);
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu();
 
         System.out.println("Welcome to CS240 Chess\n");
@@ -15,21 +19,26 @@ public class Menu {
 
         while (!Pattern.matches("(?i)quit", input) &&
                 !Pattern.matches("(?i)q", input)) {
-            menu.displayMessage(input);
-            input = scanner.nextLine();
+            menu.processInput(input);
+            input = menu.scanner.nextLine();
         }
-        menu.displayMessage(input);
+        menu.processInput(input);
     }
 
-    private void displayMessage (String input) {
+    private void processInput (String input) {
         if (Pattern.matches("(?i)help", input) ||
                 Pattern.matches("(?i)h", input)) {
             printHelp();
         } else if (Pattern.matches("(?i)quit", input) ||
                 Pattern.matches("(?i)q", input)) {
             printQuit();
+        } else if (Pattern.matches("(?i)login", input) ||
+                Pattern.matches("(?i)li", input)) {
+            login(scanner);
         }
     }
+
+
 
     private void printHelp () {
         switch (this.state) {
@@ -37,10 +46,17 @@ public class Menu {
                 System.out.println("Available Commands:");
                 System.out.println("help : h");
                 System.out.println("quit : q");
-                System.out.println("login : l");
+                System.out.println("login : li");
                 System.out.println("register : r");
                 break;
             case loggedIn:
+                System.out.println("Available Commands:");
+                System.out.println("help : h");
+                System.out.println("logout : lo");
+                System.out.println("create game : cg <gameName>");
+                System.out.println("list games : lg");
+                System.out.println("join game : jg <gameId>");
+                System.out.println("watch game : wg <gameId>");
                 break;
             case inGame:
                 break;
@@ -48,10 +64,20 @@ public class Menu {
 
     }
 
+    private void login(Scanner scanner) {
+        System.out.println("Enter your username");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password");
+        String password = scanner.nextLine();
+
+        System.out.printf("Username: %s\nPassword: %s", username, password);
+
+    }
+
     private enum STATE {
-        loggedIn,
-        loggedOut,
-        inGame
+        LOGGED_IN,
+        LOGGED_OUT,
+        IN_GAME
     }
 
     private void printQuit () {
